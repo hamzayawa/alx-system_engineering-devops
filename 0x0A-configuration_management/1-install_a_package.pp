df223ddff4dd4467b81c87cc8037ffb5e336e113
Hamza Yawa <coachbabaa@gmail.com>
2023-06-22 21:27:43 +0100

    all tasks

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# Installs flask (2.1.0)
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Kills a process named killmenow
+
+exec {'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
